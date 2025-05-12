24e3a37ada2719d1180a41a9659eb47de26abe9c
SohailUstad <67041581+SohailUstad@users.noreply.github.com>
2025-05-12 20:57:54 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open('my-cache').then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/icon-192.png',
+                '/icon-512.png'
+                // Add other files as needed (CSS, JS, etc.)
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
