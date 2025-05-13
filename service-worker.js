83c24114e4c94331b2e59694487b4560ae57e3e2
SohailUstad <67041581+SohailUstad@users.noreply.github.com>
2025-05-13 20:32:04 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,18 @@
-// Cache the important files on the first install
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('customer-app-cache').then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/icon-92.png',
-                '/bootstrap.min.css'
-            ]);
+            return cache.add('/')
+                .then(() => cache.add('/index.html'))
+                .then(() => cache.add('/icon-92.png'))
+                .then(() => cache.add('/bootstrap.min.css'))
+                .catch((error) => {
+                    console.log('Failed to cache:', error);
+                });
         })
     );
 });
 
-// Serve cached files when offline
+// Add fetch event listener to serve cached files
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((cachedResponse) => {
